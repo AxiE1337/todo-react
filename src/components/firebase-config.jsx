63b5bae96739fd558fd2d17f3ee5e6fda002e006
AxiE1337 <pixell390@gmail.com>
2021-10-30 16:57:16 +0300

    sw12

--- src/components/firebase-config.jsx
+++ src/components/firebase-config.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'process.env.REACT_APP_APIKEY',
-  authDomain: 'REACT_APP_AUTHDOMAIN',
-  projectId: 'REACT_APP_PROJECTID',
-  storageBucket: 'REACT_APP_STORAGEBUCKET',
-  messagingSenderId: 'REACT_APP_MESSAGINGSENDER',
-  appId: 'REACT_APP_APPID',
-  measurementId: 'REACT_APP_MEASUREMENT',
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDER,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENT,
 }
 
 const app = initializeApp(firebaseConfig)
